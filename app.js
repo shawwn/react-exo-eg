cf22781ffc1d82ba387dd7f79e5c63e7efac2ff8
Avaer Kazmer <a@modules.io>
2018-04-16 11:55:17 -0400

    Add controller trigger support

--- app.js
+++ app.js
@@ -13,21 +13,33 @@ class App extends React.Component {
     this.state = {
       position: '',
       rotation: '',
+      color: '#333333',
     };
 
+    const _getGamepad = () => navigator.getGamepads().find(gamepad => gamepad);
+
+    let lastTrigger = false;
     const animate = () => {
-      const gamepads = navigator.getGamepads();
-      const gamepad = gamepads.find(gamepad => gamepad);
+      const gamepad = _getGamepad();
+      let trigger = false;
       if (gamepad) {
         const p = new THREE.Vector3()
           .fromArray(gamepad.pose.position);
         const e = new THREE.Euler()
           .setFromQuaternion(new THREE.Quaternion().fromArray(gamepad.pose.orientation));
-        this.setState({
+
+        trigger = gamepad.buttons[1].pressed;
+
+        const o = {
           position: p.toArray().join(' '),
           rotation: e.toArray().slice(0, 3).map(d => d * 180 / Math.PI).join(' '), // XXX this computation is wrongly Eulerized
-        });
+        };
+        if (trigger && !lastTrigger) {
+          o.color = '#' + new THREE.Color(Math.floor(Math.random() * 0xFFFFFFFF)).getHexString();
+        }
+        this.setState(o);
       };
+      lastTrigger = trigger;
 
       window.requestAnimationFrame(animate);
     }
@@ -37,7 +49,7 @@ class App extends React.Component {
   render() {
     return <a-scene stats>
       <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" shadow />
-      <a-box position={this.state.position} rotation={this.state.rotation} depth="0.1" height="0.1" width="0.1" color="#333333" />
+      <a-box position={this.state.position} rotation={this.state.rotation} depth="0.1" height="0.1" width="0.1" color={this.state.color} />
       <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E" shadow />
       <a-cylinder
         position="1 0.75 -3"
